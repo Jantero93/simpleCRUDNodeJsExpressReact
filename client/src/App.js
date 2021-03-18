f70996dc3f10783fa5f0e02dfcaec2e570296c8a
Jani Sillanpää <jani.k.sillanpaa@tuni.fi>
2021-03-18 09:08:41 +0200

    added client

---
+++ client/src/App.js
@@ -0,0 +1,162 @@
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const [newWage, setNewWage] = useState(0);
+
+  const [emplyeesList, setEmployeesList] = useState([]);
+  /* JATKA KOHDASTA 13:30
+https://www.youtube.com/watch?v=AohARsUlwQk
+*/
+  const addEmployee = () => {
+    Axios.post("http://localhost:9000/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeesList([
+        ...emplyeesList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:9000/employees").then((response) => {
+      setEmployeesList(response.data);
+    });
+  };
+
+  const updateEmployeesWage = (id) => {
+    console.log(`id`, id);
+    Axios.put("http://localhost:9000/update", { wage: newWage, id: id }).then(
+      (reposense) => {
+        setEmployeesList(
+          emplyeesList.map((val) => {
+            return val.id === id
+              ? {
+                  id: val.id,
+                  name: val.name,
+                  country: val.country,
+                  age: val.age,
+                  position: val.position,
+                  wage: newWage,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:9000/delete/${id}`).then((response) => {
+      setEmployeesList(emplyeesList.filter((val)=> {
+        return val.id !== id;
+      }))
+    })
+  }
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name: </label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
+      <div className="seperator"></div>
+
+      <div className="employeesButton">
+        <button onClick={getEmployees}>Show Employees</button>
+
+        <div>
+          {emplyeesList.map((val, key) => {
+            return (
+              <div className="employee">
+                <div>
+                  <h3>Name: {val.name}</h3>
+                  <h3>Age: {val.age}</h3>
+                  <h3>Country: {val.country}</h3>
+                  <h3>Position: {val.position}</h3>
+                  <h3>Wage: {val.wage}</h3>
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="2000..."
+                    onChange={(event) => {
+                      setNewWage(event.target.value);
+                    }}
+                  />
+                  <button
+                    onClick={() => {
+                      updateEmployeesWage(val.id);
+                    }}
+                  >
+                    Update wage
+                  </button>
+                  <button
+                  onClick={()=> {
+                    deleteEmployee(val.id);
+                  }}>
+                    Delete 
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
